8bb052c27c5ae1dab99d29d2b8f74458daee6aa9
echo li <2654121372@qq.com>
2022-07-13 00:37:32 +0800

    更新表单格式替换邮件逻辑

--- demo/cuc/littleRedCUC/forms.py
+++ demo/cuc/littleRedCUC/forms.py
@@ -4,21 +4,21 @@ from wsgiref.validate import validator
 from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,validators
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email,Length,EqualTo
 from littleRedCUC.db_models import User
 
 # 添加邮箱唯一验证
-class Unique(object):
-    def __init__(self,model,field,message=u'该用户已存在'):
-        self.model = model
-        self.field = field
-        self.message=message
+# class Unique(object):
+#     def __init__(self,model,field,message=u'该用户已存在'):
+#         self.model = model
+#         self.field = field
+#         self.message=message
     
 
-    def __call__(self,form,field):
-        check = self.model.query.filter(self.field==field.data).first()
-        if check:
-            raise ValidationErr(self.message)
+#     def __call__(self,form,field):
+#         check = self.model.query.filter(self.field==field.data).first()
+#         if check:
+#             raise ValidationErr(self.message)
 
 class SignInForm(FlaskForm):
     email = StringField('Email  Adress', validators=[DataRequired(), Email()])
@@ -26,12 +26,11 @@ class SignInForm(FlaskForm):
 
 
 class SignUpForm(FlaskForm):
-    email = StringField('Email  Adress', validators=[DataRequired(), Email(),Unique(User,User.email)])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm',message='Passwords must match')
-    ])
-    confirm = PasswordField('Repeat Password')
+    # email = StringField('Email  Adress', validators=[DataRequired(), Email(),Unique(User,User.email)])
+    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField('密码', validators=[DataRequired(),Length(min=8,max=36)])
+    comfirm=PasswordField('确认密码',validators=[DataRequired(),EqualTo('password',message='必须和密码已输入密码相同')])
+    name= StringField('用户名',validators=[DataRequired()])
 
 
 
