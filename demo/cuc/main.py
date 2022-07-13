8b58a6e91ab3376304911dd0e574701fffd5e86d
worry333 <1092265772@qq.com>
2022-07-13 09:51:47 +0800

    实现HTTPS绑定域名

--- demo/cuc/main.py
+++ demo/cuc/main.py
@@ -3,4 +3,4 @@ from littleRedCUC import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,ssl_context=("./x.509/self-signed/self-signed-key.pem", './x.509/self-signed/key.pem'))
