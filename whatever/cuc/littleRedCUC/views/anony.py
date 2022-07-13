c1df45a586cbb4f4507e00773aa56e0e2314cd46
echo li <2654121372@qq.com>
2022-07-13 11:57:49 +0800

    增加匿名状态下注册用户名称和密码合法性限制

--- whatever/cuc/littleRedCUC/views/anony.py
+++ whatever/cuc/littleRedCUC/views/anony.py
@@ -1,11 +1,12 @@
 
 
-from flask import redirect, render_template, send_from_directory, current_app,request,flash,url_for,logging
+from flask import redirect, render_template, send_from_directory, current_app,request,flash,url_for,logging,abort
 import flask
 from flask_login import login_required
 from littleRedCUC.forms import SignUpForm
 from littleRedCUC.db_models import User,db
 from littleRedCUC.blueprints import anony
+import re
 
 @anony.route('/')
 def home():
@@ -22,12 +23,52 @@ def force_to_login():
 @anony.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignUpForm(request.form)
+    pattern=[]
+    pattern.append(re.compile('[a-z]'))
+    pattern.append(re.compile('[A-Z]'))
+    pattern.append(re.compile('[0-9]'))
+    pattern.append(re.compile('[!-/:-@[-`{-~]'))
+    namepattern=re.compile('[0-9A-Za-z\\u4E00-\\u9FFF]+')
+    threshold=0
     if request.method == 'POST' :
     # and form.validate():
-        user=User(email=form.email.data,_password=form.password.data)
-        db.session.add(user)
-        flash('welcome to littleRedCUC')
-        return redirect(url_for('auth.login'))
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            email=form.email.data
+            password=form.password.data
+            name=form.name.data
+            confirm=form.comfirm.data
+            if user:
+                flash('邮箱已存在')
+                return render_template('signup.html',form=form)
+
+            if not namepattern.fullmatch(name):
+                flash('用户名不合法')
+                return render_template('signup.html',form=form)
+
+            user = User.query.filter_by(name=form.name.data).first()
+            if user:
+                flash('用户名已存在')
+                return render_template('signup.html',form=form)
+            
+            
+            for i in range(4):
+                if pattern[i].search(password):
+                    threshold+=1
+            
+            if threshold<3:
+                flash("请使用强密码")
+                return render_template('signup.html',form=form)
+        
+            user=User(email=form.email.data,_password=form.password.data)
+            db.session.add(user)
+            flash('welcome to littleRedCUC')
+            return redirect(url_for('auth.login'))
+            
+        else:
+            flash("确认密码与密码不符")
+            return render_template('signup.html',form=form)
+
     return render_template('signup.html',form=form)
 
 
