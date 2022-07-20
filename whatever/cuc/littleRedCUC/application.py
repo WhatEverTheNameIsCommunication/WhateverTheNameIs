d10f67cd12a5a4b5c7c74a42d02ae1c7e5b120b5
echo li <2654121372@qq.com>
2022-07-20 21:37:05 +0800

    删除了作为api的post，当前实现方法是单独页面表单提交

---
+++ whatever/cuc/littleRedCUC/application.py
@@ -0,0 +1,79 @@
+# -*- coding: UTF-8 -*-
+
+import os
+from flask import Flask, redirect, url_for
+from flask.cli import AppGroup
+import click
+from pathlib import Path
+
+# from littleRedCUC.flask_adminlte import AdminLTE
+from flask import Flask
+
+from importlib import import_module
+from littleRedCUC.blueprints import all_blueprints
+
+
+from littleRedCUC.blueprints import all_blueprints
+# from littleRedCUC.views.resources import api_bp
+from littleRedCUC.extensions import login_manager,db,bcrypt,migrate
+
+from littleRedCUC.db_models import User,UserRole
+from config import config
+
+def create_app():
+    flask_app = Flask(__name__, instance_relative_config=True)
+    # AdminLTE(flask_app)
+    flask_app.config['SECRET_KEY']='littleRedCUC'
+    flask_app.config['SERVER_NAME']='WhateverItIs.cuc.edu.cn:5000'
+    config_name = os.getenv('FLASK_CONFIG', 'default')
+    flask_app.config.from_object(config[config_name])
+    flask_app.config.from_pyfile('app.cfg', silent=True)
+
+    upload_path = Path(flask_app.instance_path) / 'upload'
+    if not Path(flask_app.instance_path).exists():
+        Path(flask_app.instance_path).mkdir()
+    if not upload_path.exists():
+        upload_path.mkdir()
+    
+
+    flask_app.config['UPLOAD_FOLDER'] = str(upload_path)
+
+    login_manager.session_protection = 'AdminPassword4Me'
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = 'Unauthorized User.'
+    login_manager.login_message_category = "info"
+
+    login_manager.init_app(flask_app)
+    db.init_app(flask_app)
+    bcrypt.init_app(flask_app)
+    migrate.init_app(flask_app, db)
+
+    for bp in all_blueprints:
+        import_module(bp.import_name)
+        flask_app.register_blueprint(bp)
+    # flask_app.register_blueprint(api_bp)
+
+    user_cli = AppGroup('user')
+
+    @user_cli.command('create-admin')
+    @click.argument('email')
+    @click.argument('name')
+    @click.argument('password')
+    
+    def create_admin(email, name, password):
+        user = User(email=email,
+                    # email_confirmed=True,
+                    name=name,
+                    role=UserRole.ADMIN)
+        user.password = password
+
+        db.session.add(user)
+        db.session.commit()
+
+    flask_app.cli.add_command(user_cli)
+
+    return flask_app
+
+if __name__ == "__main__":
+    flask_app = create_app()
+    flask_app.run(host='0.0.0.0',port=5000, debug=True,ssl_context=("../self-signed/self-signed-key.pem", '../self-signed/key.pem'))
---
+++ whatever/cuc/littleRedCUC/db_models.py
@@ -0,0 +1,157 @@
+from flask_login import UserMixin,current_user
+from sqlalchemy.ext.hybrid import hybrid_property
+from flask_restful import Resource, reqparse
+from flask import request, current_app,redirect,url_for
+
+
+from datetime import datetime
+
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
+from datetime import datetime
+from pathlib import Path
+
+from littleRedCUC.extensions import db,marshmallow
+from littleRedCUC.extensions import bcrypt
+from littleRedCUC.utils.paginate import paginate
+
+
+
+import enum
+
+class UserRole(enum.Enum):
+    ADMIN = 'Administrator'
+    USERS = 'Normal users'
+
+# user models
+class User(db.Model, UserMixin):
+
+
+    __tablename__ = "users"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=True)
+    name = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.Enum(UserRole), default=UserRole.USERS)
+    _password = db.Column(db.String(128), nullable=False)
+    created_time = db.Column(db.DateTime, default=datetime.now)
+
+
+    # def __init__(self,email,password,role=UserRole.USERS):
+    #     self.email = email.lower()
+    #     # self._password(password)
+    #     self.role=role
+
+
+
+
+        
+
+
+    @hybrid_property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, plaintext):
+        self._password = bcrypt.generate_password_hash(plaintext)
+
+
+
+
+    def is_correct_password(self, plaintext):
+        return bcrypt.check_password_hash(self._password, plaintext)
+
+    def __repr__(self):
+        return "<User %r>" % self.name
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_time = db.Column(db.DateTime, default=datetime.now)
+    user_name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.Text)
+    filename = db.Column(db.String(255), nullable=False)
+
+
+
+
+# class PostSchema(marshmallow.SQLAlchemyAutoSchema):
+
+#     id = marshmallow.Int(dump_only=True)
+
+#     class Meta:
+#         model = Post
+#         sql_session = db.session
+
+
+
+# class PostResource(Resource):
+#     def get(self, post_id):
+#         schema = PostSchema()
+#         post = Post.query.get_or_404(post_id)
+#         return {"post": schema.dump(user).data}
+
+#     def put(self, post_id):
+#         schema = PostSchema(partial=True)
+#         post = Post.query.get_or_404(post_id)
+#         post, errors = schema.load(request.json, instance=post)
+#         if errors:
+#             return errors, 422
+
+#         db.session.commit()
+        
+#         return {"msg": "post updated", "post": schema.dump(post).data}
+
+    
+#     def delete(self, post_id):
+#         post = post.query.get_or_404(post_id)
+#         db.session.delete(post)
+#         db.session.commit()
+
+#         return {"msg": "post deleted"}
+    
+
+# post_parser = reqparse.RequestParser()
+# post_parser.add_argument('text', type=str, location='form')
+# post_parser.add_argument('image', type=FileStorage, location='files')
+
+
+# class PostList(Resource):
+#     def get(self):
+#         schema = PostSchema(many=True)
+#         query = Post.query
+#         return paginate(query, schema)
+
+#     def post(self):
+#         schema = PostSchema()
+
+#         args = post_parser.parse_args()
+
+#         text = args.get('text')
+#         if text is None:
+#             text = ''
+
+#         image = args.get('image')
+#         if image is None:
+#             return {'msg': 'you must post file.'}, 422
+
+#         file_name = str(int(datetime.now().timestamp() *1000)) + '-' + secure_filename(image.filename)
+#         image.save(str(Path(current_app.config['UPLOAD_FOLDER']) / file_name))
+#         try:
+#             post = Post(user_id=current_user.id,
+#                         user_name=current_user.name,
+#                         text=text,
+#                         image=file_name)
+
+#             db.session.add(post)
+#             db.session.commit()
+#             return redirect(url_for('auth.layout'))
+#             # return {"msg": "post created", "post": schema.dump(post)}, 201
+#         except:
+#             return redirect(url_for('auth.login'))
